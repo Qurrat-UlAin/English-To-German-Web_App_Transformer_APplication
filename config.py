8594e7523f72ab4d1e88349f73fd9e1c8d74dd38
Qurrat Ul Ain <120576730+Qurrat-UlAin@users.noreply.github.com>
2025-04-19 14:31:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ def get_config():
         "batch_size": 4,
         "num_epochs": 20,
         "lr": 10**-4,
-        "seq_len": 512,
+        "seq_len": 128,
         "d_model": 512,
         "datasource": 'opus_books',
         "lang_src": "de",
